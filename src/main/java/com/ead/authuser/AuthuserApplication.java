5b745032dd797605f248446b3d6c7730a5e827b7
Ricardo Tonietti <rictonietti@gmail.com>
2022-04-07 17:07:07 -0300

    creat configuration Eureka client, ajust pom and yaml

--- src/main/java/com/ead/authuser/AuthuserApplication.java
+++ src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -2,11 +2,13 @@ package com.ead.authuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
 @EnableSwagger2WebMvc
+@EnableEurekaClient
 public class AuthuserApplication {
 
 	public static void main( String[] args) {
