60d6f4137ac5766d9af6ea98a6f64177b73026f7
Ricardo Tonietti <rictonietti@gmail.com>
2022-04-26 14:26:41 -0300

    configuration global, config-server

--- src/main/java/com/ead/authuser/AuthuserApplication.java
+++ src/main/java/com/ead/authuser/AuthuserApplication.java
@@ -3,10 +3,8 @@ package com.ead.authuser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
 @EnableEurekaClient
 public class AuthuserApplication {
 
